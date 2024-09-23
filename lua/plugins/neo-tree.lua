97fad655621263e41c976c717241ac3ce5daa0e3
c3-daniel-choi <daniel.choi@c3.ai>
2024-09-23 17:20:07 -0400

    added git-signs

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end
 }
